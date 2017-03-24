536bdc3c6b69c8536a5cb0aa1db1e5e01b557910
Jontahan <jona.jorg@gmail.com>
2017-03-24 17:44:17 +0100

    map lambda magic

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class Satellite:
         self.z = z
         self.t = t
 
-c = 299792.458
+c = 299792.458 # speed of light
 
 satellites = [Satellite(15600, 7540, 20140, 0.07074),
               Satellite(18760, 2750, 18610, 0.07220),
@@ -20,48 +20,33 @@ y = Symbol('y')
 z = Symbol('z')
 d = Symbol('d')
 
-symfuns = []
+symfuns = list(map(lambda s : (x - s.x)**2 +\
+                              (y - s.y)**2 +\
+                              (z - s.z)**2 -\
+                              (c * (s.t - d))**2, satellites))
 
-for s in satellites:
-    symfuns.append( (x - s.x)**2 +\
-                    (y - s.y)**2 +\
-                    (z - s.z)**2 -\
-                    (c * (s.t - d))**2)
-    
 def F(p_x,p_y,p_z,p_d):
-    return [lambdify((x,y,z,d), symfuns[0])(p_x,p_y,p_z,p_d),
-            lambdify((x,y,z,d), symfuns[1])(p_x,p_y,p_z,p_d),
-            lambdify((x,y,z,d), symfuns[2])(p_x,p_y,p_z,p_d),
-            lambdify((x,y,z,d), symfuns[3])(p_x,p_y,p_z,p_d)]
+    return  list(map(lambda f : lambdify((x,y,z,d), f)(p_x,p_y,p_z,p_d), symfuns))
 
 DF_list = [];
 
 for f in symfuns:
     DF_list.append([lambdify(x, f.diff(x), 'numpy'),
-               lambdify(y, f.diff(y), 'numpy'),
-               lambdify(z, f.diff(z), 'numpy'),
-               lambdify(d, f.diff(d), 'numpy')])
+                    lambdify(y, f.diff(y), 'numpy'),
+                    lambdify(z, f.diff(z), 'numpy'),
+                    lambdify(d, f.diff(d), 'numpy')])
 
-def DF(p_x,p_y,p_z,p_d):
+def DF(vec):
     out = []
     for f_vec in DF_list:
-        out.append([f_vec[0](p_x),
-                    f_vec[1](p_y),
-                    f_vec[2](p_z),
-                    f_vec[3](p_d)])
+        tmp = []
+        for i in range(0, 4):
+            tmp.append(f_vec[i](vec[i]))    
+        out.append(tmp)
     return out
 
-x0 = [0,0,6370,0]
+x0 = [0,0,6370,0] # initial vector
 
-for i in range(0, 5):
-    x0 = x0 - np.dot(np.linalg.inv(DF(x0[0],x0[1],x0[2],x0[3])), F(x0[0],x0[1],x0[2],x0[3]))
+for i in range(1, 11):
+    x0 = x0 - np.dot(np.linalg.inv(DF(x0)), F(x0[0], x0[1], x0[2], x0[3]))
     print('iter #', i, ': ', x0)
-    
-#ff = lambdify((x, y, z, d), funs[0])
-#print(ff(1,2,3,4))
-#f1prime = funs[0].diff(x)
-#print(f1prime)
-#f = lambdify(x, f1prime, 'numpy')
-#print(f(5))
-
-
