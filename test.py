2c0bd197377b3795b4f8238cd1e1f9cc955af2c8
Jontahan <jona.jorg@gmail.com>
2017-03-30 11:22:11 +0200

    made symbolic variable as tuple, shorter code

--- test.py
+++ test.py
@@ -15,20 +15,17 @@ satellites = [Satellite(15600, 7540, 20140, 0.07074),
               Satellite(17610, 14630, 13480, 0.07690),
               Satellite(19170, 610, 18390, 0.07242)]
 
-x = Symbol('x')
-y = Symbol('y')
-z = Symbol('z')
-d = Symbol('d')
+syms = (Symbol('x'), Symbol('y'), Symbol('z'), Symbol('d'))
 
-symfuns = list(map(lambda s : (x - s.x)**2 +\
-                              (y - s.y)**2 +\
-                              (z - s.z)**2 -\
-                              (c * (s.t - d))**2, satellites))
+symfuns = list(map(lambda s : (syms[0] - s.x)**2 +\
+                              (syms[1] - s.y)**2 +\
+                              (syms[2] - s.z)**2 -\
+                              (c * (s.t - syms[3]))**2, satellites))
 
 def F(vec):
-    return list(map(lambda f : lambdify((x,y,z,d), f)(vec[0],vec[1],vec[2],vec[3]), symfuns))
+    return list(map(lambda f : lambdify(syms, f)(vec[0], vec[1], vec[2], vec[3]), symfuns))
 
-jacobi = list(map(lambda f : list(map(lambda s : lambdify(s, f.diff(s), 'numpy'), [x,y,z,d])), symfuns))
+jacobi = list(map(lambda f : list(map(lambda s : lambdify(s, f.diff(s), 'numpy'), syms)), symfuns))
 
 def DF(vec):
     return list(map(lambda f_vec : list(map(lambda i : f_vec[i](vec[i]), range(0, 4))),jacobi))
