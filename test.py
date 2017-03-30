72f12ecc563536fa72336960a788d0261ef8e1b1
Jontahan <jona.jorg@gmail.com>
2017-03-30 13:16:10 +0200

    defined newtons as a reusable function, pulled out inputs

--- test.py
+++ test.py
@@ -8,30 +8,33 @@ class Satellite:
         self.z = z
         self.t = t
 
-c = 299792.458 # speed of light
+c = 299792.458
+
+def newtons_satellites(satellites, x0):
+    syms = (Symbol('x'), Symbol('y'), Symbol('z'), Symbol('d'))
+
+    symfuns = list(map(lambda s : (syms[0] - s.x)**2 +\
+                   (syms[1] - s.y)**2 +\
+                   (syms[2] - s.z)**2 -\
+                   (c * (s.t - syms[3]))**2, satellites))
+    def F(vec):
+        return list(map(lambda f : lambdify(syms, f)(vec[0], vec[1], vec[2], vec[3]), symfuns))
+
+    jacobi = list(map(lambda f : list(map(lambda s : lambdify(s, f.diff(s), 'numpy'), syms)), symfuns))
+    
+    def DF(vec):
+        return list(map(lambda f_vec : list(map(lambda i : f_vec[i](vec[i]), range(0, 4))),jacobi))
+
+    for i in range(0, 10):
+        x0 -= np.dot(np.linalg.inv(DF(x0)), F(x0))
+        print('iter #', i, ': ', x0)
+
 
 satellites = [Satellite(15600, 7540, 20140, 0.07074),
               Satellite(18760, 2750, 18610, 0.07220),
               Satellite(17610, 14630, 13480, 0.07690),
               Satellite(19170, 610, 18390, 0.07242)]
 
-syms = (Symbol('x'), Symbol('y'), Symbol('z'), Symbol('d'))
+init_vec = [0,0,6370,0]
 
-symfuns = list(map(lambda s : (syms[0] - s.x)**2 +\
-                              (syms[1] - s.y)**2 +\
-                              (syms[2] - s.z)**2 -\
-                              (c * (s.t - syms[3]))**2, satellites))
-
-def F(vec):
-    return list(map(lambda f : lambdify(syms, f)(vec[0], vec[1], vec[2], vec[3]), symfuns))
-
-jacobi = list(map(lambda f : list(map(lambda s : lambdify(s, f.diff(s), 'numpy'), syms)), symfuns))
-
-def DF(vec):
-    return list(map(lambda f_vec : list(map(lambda i : f_vec[i](vec[i]), range(0, 4))),jacobi))
-
-x0 = [0,0,6370,0] # initial vector
-
-for i in range(0, 10):
-    x0 = x0 - np.dot(np.linalg.inv(DF(x0)), F(x0))
-    print('iter #', i, ': ', x0)
+newtons_satellites(satellites, init_vec);
