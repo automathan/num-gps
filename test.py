9aa0f0531db588427b115c36f1540e2f8edce7b0
Jontahan <jona.jorg@gmail.com>
2017-04-06 15:50:14 +0000

    spherical newton

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 from sympy import *
 import numpy as np
+import math
+
+
+c = 299792.458
 
 class Satellite:
     def __init__(self, x, y, z, t):
@@ -7,9 +11,7 @@ class Satellite:
         self.y = y
         self.z = z
         self.t = t
-
-c = 299792.458
-
+        
 def newtons_satellites(satellites, x0):
     syms = (Symbol('x'), Symbol('y'), Symbol('z'), Symbol('d'))
 
@@ -21,14 +23,24 @@ def newtons_satellites(satellites, x0):
         return list(map(lambda f : lambdify(syms, f)(vec[0], vec[1], vec[2], vec[3]), symfuns))
 
     jacobi = list(map(lambda f : list(map(lambda s : lambdify(s, f.diff(s), 'numpy'), syms)), symfuns))
-    
+
     def DF(vec):
         return list(map(lambda f_vec : list(map(lambda i : f_vec[i](vec[i]), range(0, 4))),jacobi))
-
+    
     for i in range(0, 10):
         x0 -= np.dot(np.linalg.inv(DF(x0)), F(x0))
         print('iter #', i, ': ', x0)
+    return x0
 
+def newtons_satellites_spherical(coords, x0, rho):
+    return newtons_satellites(list(map(lambda coord :
+                                Satellite(rho*cos(coord[0])*cos(coord[1]),
+                                          rho*cos(coord[0])*sin(coord[1]),
+                                          rho*sin(coord[0]),
+                                          x0[3] + sqrt((rho*cos(coord[0])*cos(coord[1]))**2 +
+                                                   (rho*cos(coord[0])*sin(coord[1]))**2 +
+                                                   (rho*sin(coord[0]) - x0[2])**2)/c),
+                                       coords)), x0)
 
 satellites = [Satellite(15600, 7540, 20140, 0.07074),
               Satellite(18760, 2750, 18610, 0.07220),
@@ -37,4 +49,14 @@ satellites = [Satellite(15600, 7540, 20140, 0.07074),
 
 init_vec = [0,0,6370,0]
 
-newtons_satellites(satellites, init_vec);
+print('\nsolution : ', newtons_satellites(satellites, init_vec), '\n\n')
+
+coords = []
+
+for i in range(0, 4):
+    coords.append([np.random.uniform(0, math.pi / 2), np.random.uniform(0, 2 * math.pi)])
+
+init_vec = [0,0,6370,0.0001]
+
+print('\nsolution : ', newtons_satellites_spherical(coords, init_vec, 26570))
+
